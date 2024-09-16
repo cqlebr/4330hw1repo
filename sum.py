d8dcde014504623da0e61207b8528d27a01780dd
Caleb <cqlebrg@gmail.com>
2024-09-16 16:46:58 -0500

    Sum number file

---
+++ sum.py
@@ -0,0 +1,7 @@
+print("Must run in Terminal to input numbers!")
+firstNum = input("Enter your first number: ")
+firstNum = int(firstNum)
+secondNum = input("Enter your second number: ")
+secondNum = int(secondNum)
+print("The sum of your two numbers are: ",firstNum + secondNum)
+    
