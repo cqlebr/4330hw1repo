a7662a6675682575246b67b9d627e2745f9b3d8c
Caleb <cqlebrg@gmail.com>
2024-09-16 16:46:47 -0500

    Difference number file

---
+++ difference.py
@@ -0,0 +1,6 @@
+print("Must run in Terminal to input numbers!")
+firstNum = input("Enter your first number: ")
+firstNum = int(firstNum)
+secondNum = input("Enter your second number: ")
+secondNum = int(secondNum)
+print("The difference of your numbers are: ",firstNum - secondNum)
